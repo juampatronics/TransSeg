ccbc184f578509afe219be1fdcf58a25b02f4c88
Yuhui Zhang <zyh.thu@gmail.com>
2022-03-14 18:11:42 -0700

    update memory

--- src/scripts/train_msd_07.sh
+++ src/scripts/train_msd_07.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=msd
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=6gb
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=4gb
 #SBATCH --partition=pasteur
 #SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
